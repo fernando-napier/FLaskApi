8596638365c25055e8e239e87fa167190b81caf3
unknown <fnapier@hayneedle.com>
2021-07-02 11:13:26 -0500

    separating out the hello world app to a separate folder

--- api.py
+++ api.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from app.hello_world import HelloWorld
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/hello_world', '/', '/hello')
 
 if __name__ == '__main__':
     app.run(debug=True)
